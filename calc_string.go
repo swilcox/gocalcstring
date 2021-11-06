fd9dadb1f491ddd1a4eea25865b87a645bae7419
swilcox <steven@wilcoxzone.com>
2021-11-06 10:48:14 -0500

    passing more tests

--- calc_string.go
+++ calc_string.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"errors"
+	"regexp"
+	"strconv"
+)
+
 func CalcString(i string) (result int, err error) {
-	return 0, nil
+	result = 0
+	reg := regexp.MustCompile(`[,\n]`)
+	subStrings := reg.Split(i, -1)
+	// subStrings := strings.Split(i, ",")
+
+	for _, s := range subStrings {
+		if s != "" {
+			value, err := strconv.Atoi(s)
+			if err != nil {
+				return 0, err
+			}
+			if value < 0 {
+				return 0, errors.New("bad")
+			}
+			if value <= 1000 {
+				result += value
+			}
+		}
+	}
+	return result, nil
 }
--- calc_string_test.go
+++ calc_string_test.go
@@ -30,14 +30,10 @@ func TestCalcStringNewLine(t *testing.T) {
 	}
 }
 
-func TestCalcString3Numbers(t *testing.T) {
-	result, err := CalcString("20\n21\n23")
+func TestCalcStringMixedDelimiters(t *testing.T) {
+	result, err := CalcString("20\n21,23")
 	if result != 64 || err != nil {
-		t.Errorf("expected 64 result and nil error from 3 number newline string but got: %q, %q", result, err)
-	}
-	result, err = CalcString("20,21,23")
-	if result != 64 || err != nil {
-		t.Errorf("expected 64 result and nil error from 3 number comma string but got: %q, %q", result, err)
+		t.Errorf("expected 64 result and nil error from 3 number mixed delimiter string but got: %q, %q", result, err)
 	}
 }
 
@@ -52,5 +48,26 @@ func TestCalcStringIgnoreGreater1000(t *testing.T) {
 	result, err := CalcString("42\n1001")
 	if result != 42 || err != nil {
 		t.Errorf("expected 42 result and nil error from string with greater than 1000 in it but got: %q, %q", result, err)
+	}
+}
+
+func TestCalcStringCustomDelimiter(t *testing.T) {
+	result, err := CalcString("//;\n1;2")
+	if result != 3 || err != nil {
+		t.Errorf("expected 3 result and nil error from string with custom ; delimiter: %q, %q", result, err)
+	}
+}
+
+func TestCalcStringCustomLengthDelimiter(t *testing.T) {
+	result, err := CalcString("//[***]\n1***2***3")
+	if result != 6 || err != nil {
+		t.Errorf("expected 6 result and nil error from string with custom *** delimiter: %q, %q", result, err)
+	}
+}
+
+func TestCalcStringCustomLengthMultipleDelimiters(t *testing.T) {
+	result, err := CalcString("//[*][%]\n1*2%3")
+	if result != 6 || err != nil {
+		t.Errorf("expected 6 result and nil error from string with custom delimiters: %q, %q", result, err)
 	}
 }
