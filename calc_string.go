56010917b5f5c676a32c3cd70dda959ca3d05e79
Steven Wilcox <steven@wilcoxzone.com>
2021-11-07 14:25:08 -0600

    all tests passing

--- calc_string.go
+++ calc_string.go
@@ -4,13 +4,32 @@ import (
 	"errors"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func CalcString(i string) (result int, err error) {
 	result = 0
-	reg := regexp.MustCompile(`[,\n]`)
-	subStrings := reg.Split(i, -1)
-	// subStrings := strings.Split(i, ",")
+	compStr := i
+	reg := regexp.MustCompile(`,|\n`)
+	delreg := regexp.MustCompile(`^//(?P<delimiters>.+)\n`)
+	matchArray := delreg.FindStringSubmatch(i)
+	if matchArray != nil {
+		subReg := regexp.MustCompile(`\[(.+?)]`)
+		delimiterArray := subReg.FindAllStringSubmatch(matchArray[1], -1)
+
+		if delimiterArray != nil {
+			delimiters := []string{}
+			for _, match := range delimiterArray {
+				delimiters = append(delimiters, regexp.QuoteMeta(match[1]))
+			}
+			reg = regexp.MustCompile(strings.Join(delimiters, "|"))
+		} else {
+			reg = regexp.MustCompile(regexp.QuoteMeta(matchArray[1]))
+		}
+
+		compStr = strings.SplitN(i, "\n", 2)[1]
+	}
+	subStrings := reg.Split(compStr, -1)
 
 	for _, s := range subStrings {
 		if s != "" {
